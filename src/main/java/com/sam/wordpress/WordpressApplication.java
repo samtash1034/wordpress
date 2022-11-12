2855e728f536ebf393686c0a9b7d03e20d8c1aae
samtash1034 <samtash1034@gmail.com>
2022-11-12 14:47:35 +0800

    11/12

---
+++ src/main/java/com/sam/wordpress/WordpressApplication.java
@@ -0,0 +1,13 @@
+package com.sam.wordpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/wordpress/WordpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.wordpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
